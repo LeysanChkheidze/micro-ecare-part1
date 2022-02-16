240ac9155e75d5a14795631f2721ea2b261703ed
Leysan Chkheidze <lejsanc@yandex.ru>
2022-02-16 15:02:12 +0300

    initial spring boot app

---
+++ src/main/java/com/javaschool/microecare/MicroEcareApplication.java
@@ -0,0 +1,13 @@
+package com.javaschool.microecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaschool/microecare/MicroEcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaschool.microecare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
