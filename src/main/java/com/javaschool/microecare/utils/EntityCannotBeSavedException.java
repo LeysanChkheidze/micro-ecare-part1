9faadf127b38eab607c0cbc0df5c126e3994e5fb
Leysan Chkheidze <lejsanc@yandex.ru>
2022-02-19 12:11:18 +0300

    Javadocs for EntityCannotBeSavedException and EntityNotFoundInDBException

--- src/main/java/com/javaschool/microecare/utils/EntityCannotBeSavedException.java
+++ src/main/java/com/javaschool/microecare/utils/EntityCannotBeSavedException.java
@@ -4,19 +4,38 @@ import org.springframework.dao.DataIntegrityViolationException;
 
 //TODO: подумать, куда положить этот эксепшн
 
+/**
+ * The type EntityCannotBeSavedException which extends DataIntegrityViolationException and additionally has a field to store entity name.
+ */
 public class EntityCannotBeSavedException extends DataIntegrityViolationException {
     private String entityName;
 
 
+    /**
+     * Instantiates a new Entity cannot be saved exception.
+     *
+     * @param msg the exception message
+     */
     public EntityCannotBeSavedException(String msg) {
         super(msg);
     }
 
+    /**
+     * Instantiates a new Entity cannot be saved exception.
+     *
+     * @param entityName the entity name for which exception was thrown in human-readable form
+     * @param msg        the exception message
+     */
     public EntityCannotBeSavedException(String entityName, String msg) {
         this(msg);
         this.entityName = entityName;
     }
 
+    /**
+     * Gets entity name.
+     *
+     * @return the entity name
+     */
     public String getEntityName() {
         return entityName;
     }
--- src/main/java/com/javaschool/microecare/utils/EntityNotFoundInDBException.java
+++ src/main/java/com/javaschool/microecare/utils/EntityNotFoundInDBException.java
@@ -2,28 +2,61 @@ package com.javaschool.microecare.utils;
 
 import javax.persistence.EntityNotFoundException;
 
+/**
+ * The type EntityNotFoundInDBException extends EntityNotFoundException and additionally stores id of the searched entity and entity name.
+ */
 public class EntityNotFoundInDBException extends EntityNotFoundException {
+    /**
+     * The Id.
+     */
     long id;
+    /**
+     * The Entity name.
+     */
     String entityName;
 
+    /**
+     * Instantiates a new EntityNotFoundInDBException.
+     *
+     * @param message the exception message
+     */
     public EntityNotFoundInDBException(String message) {
         super(message);
     }
 
+    /**
+     * Instantiates a new Entity not found in db exception.
+     */
     public EntityNotFoundInDBException() {
         super();
     }
 
+    /**
+     * Instantiates a new Entity not found in db exception.
+     *
+     * @param id         the id of entity which wasn't found
+     * @param entityName the name of entity which wasn't found
+     */
     public EntityNotFoundInDBException(long id, String entityName) {
         super(String.format("%s with id %d isn't found", entityName, id));
         this.id = id;
         this.entityName = entityName;
     }
 
+    /**
+     * Gets id.
+     *
+     * @return the id
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     * Gets entity name.
+     *
+     * @return the entity name
+     */
     public String getEntityName() {
         return entityName;
     }
